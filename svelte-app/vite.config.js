2c56e6d326aa925a99831cb12c85ff28af0f987a
Brian Blosser <bpb54321@gmail.com>
2023-06-30 14:11:26 -0400

    Update base path.

--- svelte-app/vite.config.js
+++ svelte-app/vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/presta-shop/modules/mycarrousel/views/assets/',
   build: {
     outDir: '../views/assets/',
     emptyOutDir: true,
