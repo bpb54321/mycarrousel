3aaaf87a01bab4c037db4a81fe51b4a91aa91771
Brian Blosser <bpb54321@gmail.com>
2023-07-04 12:00:30 -0400

    Add more details to test of Decathlon carrousel.

--- svelte-app/tests/mycarrousel.spec.ts
+++ svelte-app/tests/mycarrousel.spec.ts
@@ -10,4 +10,20 @@ test.describe('My Carrousel', () => {
 
         await expect(myCarrouselTitle).toBeVisible()
     })
+    test('should display the correct images when the next button is clicked', async ({page}) => {
+        // Arrange
+        // Act
+        await page.goto('https://www.decathlon.ca/fr')
+
+        // Assert
+        const recommandationsCarrousel = await page.getByTestId('CarouselLayout_wrapper').filter({ hasText: 'Nos recommandations' })
+        await expect(recommandationsCarrousel).toBeVisible()
+
+        const carrouselHeading = await recommandationsCarrousel.getByRole('heading', {name: 'Nos recommandations', level: 2})
+        await expect(carrouselHeading).toBeVisible()
+
+        // const image = await
+
+            // https://images.ctfassets.net/tacg73mas1jx/1J4L9xGRfwEB7wIU53jI8r/86349fc8428647621275241e817e3849/SLIDER_-_FESTIVAL_FR.jpg
+    })
 })
