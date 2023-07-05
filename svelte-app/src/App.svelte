44475be7b689ad01640c368ef84c813b677a09c0
Brian Blosser <bpb54321@gmail.com>
2023-07-05 11:20:59 -0400

    WIP - Update test for multiple images.

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,11 +1,19 @@
 <script>
+    let images = [
+        {
+            altText: "Deux femmes qui dansent",
+            url: "https://images.ctfassets.net/tacg73mas1jx/1J4L9xGRfwEB7wIU53jI8r/86349fc8428647621275241e817e3849/SLIDER_-_FESTIVAL_FR.jpg",
+        },
+    ]
 </script>
 
 <div data-testid="my-carrousel">
     <h2>Nos recommandations</h2>
+    {#each images as image}
     <div>
         <img src="https://images.ctfassets.net/tacg73mas1jx/1J4L9xGRfwEB7wIU53jI8r/86349fc8428647621275241e817e3849/SLIDER_-_FESTIVAL_FR.jpg" alt="Deux femmes qui dansent">
     </div>
+    {/each}
 </div>
 
 <style>
--- svelte-app/tests/mycarrousel.spec.ts
+++ svelte-app/tests/mycarrousel.spec.ts
@@ -21,8 +21,31 @@ test.describe('My Carrousel', () => {
         const carrouselHeading = await recommendationsCarrousel.getByRole('heading', {name: 'Nos recommandations', level: 2})
         await expect(carrouselHeading).toBeVisible()
 
-        const image = await recommendationsCarrousel.getByAltText('Deux femmes qui dansent')
+        const expectedAltTexts = [
+            "Deux femmes qui dansent",
+            "Un homme qui court sur un pont",
+            "Un enfant qui fait du vélo à la campagne",
+            "Une fille qui saute sur un trampoline",
+            "Un groupe de femmes qui se parlent en costume de bain",
+            "Vue de derrière d'un homme et d'une femme qui portent des sacs à dos",
+            "Trois amis qui s'assoient sur leur planches à pagaie dans une crique",
+            "Une femme met un matelas dans une tente",
+            "Une femme s'assoit dans une tente",
+            "Un gros plan du pied droit de quelqu'un portant une chaussure d'eau",
+            "Un homme et une femme s'assoyant sur le bord d'une piscine avec des serviettes drapées sur leurs épaules",
+            "Le visage d'une femme qui porte des luntettes de soleil avec le soleil qui se couche derrière elle"
+        ]
 
-        await expect(image).toHaveAttribute('src', 'https://images.ctfassets.net/tacg73mas1jx/1J4L9xGRfwEB7wIU53jI8r/86349fc8428647621275241e817e3849/SLIDER_-_FESTIVAL_FR.jpg')
+        const expectedUrls = [
+            'https://images.ctfassets.net/tacg73mas1jx/1J4L9xGRfwEB7wIU53jI8r/86349fc8428647621275241e817e3849/SLIDER_-_FESTIVAL_FR.jpg',
+            'https://images.ctfassets.net/tacg73mas1jx/CG8U8nDLqEDtws1ms0qqc/895af2057f9d152a106aa1ec12fdb53a/SLIDER_LIQUIDATION_HOMME_FR.jpg'
+        ]
+
+        for (let i = 0; i < 2; i++) {
+            const expectedAltText = expectedAltTexts[i]
+            const expectedUrl = expectedUrls[i]
+            const image = await recommendationsCarrousel.getByAltText(expectedAltText)
+            await expect(image).toHaveAttribute('src', expectedUrl)
+        }
     })
 })
